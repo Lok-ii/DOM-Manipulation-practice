45712cbab0235538a7f6cb698cb5b0e82c256197
Lok-ii <lokeshkataria.work@gmail.com>
2023-10-02 21:11:59 +0530

    Learned some new commands

---
+++ script.js
@@ -0,0 +1,9 @@
+let container = document.getElementById("container");
+
+container.style.backgroundColor = "yellow";
+container.style.margin = "20px 20px 20px 20px";
+container.style.padding = "10px 10px 10px 10px";
+container.style.fontSize = "18px";
+container.style.fontWeight = 700;
+container.style.height = "200px";
+container.style.width = "300px";
